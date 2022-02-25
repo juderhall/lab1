14a74691ab8bf0779e9e8084477d025f211c1267
Jude Hall <jude.rendel.hall@gmail.com>
2022-02-24 20:02:45 -0700

    Problems 5 - 7 completed

--- lab.js
+++ lab.js
@@ -1,4 +1,5 @@
 ////////// PROBLEM 1 //////////
+
 /*
   Create a variable called 'lovesCode' and set it equal to true. 
   Check to see if 'lovesCode' is equal to true or false. 
@@ -15,6 +16,7 @@ if (LovesCode === true) {
 }
 
 // For problems 2-3 use the following lines of code:
+
 var amysAge = 29
 var brittanisAge = 34
 var amysBirthYear = 1991
@@ -35,7 +37,6 @@ if (amysAge > brittanisAge) {
   console.log("Amy and Brittani are the same age.")
 }
 
-
 ////////// PROBLEM 3 //////////
 
 /*
@@ -48,7 +49,6 @@ if (amysBirthYear === brittanisBirthYear) {
 } else {
   console.log("Amy & Brittani were not born in the same year.")
 }
-
 
 ////////// PROBLEM 4 //////////
 
@@ -86,16 +86,15 @@ if (temperature < 60) {
   }
 }
 
-
-
 ////////// PROBLEM 5 //////////
 
 /*
   Create a for-loop that runs 10 times, and on each iteration logs the word 'hello'.
 */
 
-//CODE HERE
-
+for (let i = 0; i < 10; i++) {
+  console.log("hello")
+}
 
 ////////// PROBLEM 6 //////////
 
@@ -103,8 +102,9 @@ if (temperature < 60) {
   Create a for-loop that runs 10 times, logging the numbers 1 through 10. Ex, on the first iteration it would log 1, on the second iteration it would log 2.
 */
 
-//CODE HERE
-
+for (let i = 0; i < 10; i++) {
+  console.log(i + 1)
+}
 
 ////////// PROBLEM 7 //////////
 
@@ -112,12 +112,14 @@ if (temperature < 60) {
   Create a for-loop that logs the numbers 0 through 10 backwards. Ex, on the first iteration it would log 10, on the second iteration it would log 9.
 */
 
-//CODE HERE
+for (let i = 0; i < 10; i++) {
+  console.log(10 - i)
+}
+
+////////// PROBLEM 8 //////////
 
 let score = 0
 let passingScore = 7
-
-////////// PROBLEM 8 //////////
 
 /*
   Create a while-loop that logs "Your score is not high enough" while the score variable above is below passingScore. In each iteration of the loop, increase the score by one. If your code is successful, you will see your logged statement 7 times.
@@ -128,26 +130,26 @@ let passingScore = 7
 ////////// INTERMEDIATE PROBLEMS //////////
 
 ////////// PROBLEM 9 //////////
+
 /*
   Create a variable called 'changeMyMind' and set it equal to true. 
   Check to see if changeMyMind is set to true or false, if it is true, change the status to false, if it is false, change the status to true.
-
 */
 
 //CODE HERE
 
 ////////// PROBLEM 10 //////////
+
 // Try to change the value of changeMyMind (irrespective of whether it is true or false) and then console.log it's new status. It should log true. Hint: use the not operator.
 
 //CODE HERE
-
-
 
 ////////// ADVANCED PROBLEMS //////////
 
 ////////// PROBLEM 11 //////////
 
 let z = 5
+
 // Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
 //CODE HERE
